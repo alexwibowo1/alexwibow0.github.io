57d78d496215a02e62b9ed2f2a17a25396dc9148
unknown <alexwibowo719@gmail.com>
2022-06-28 18:27:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Student', 'In The Pamulang', 'University'];
+let count = 10;
+let txtIndex = 10;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
